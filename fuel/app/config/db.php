be77a809a7859a376fdc95f754a8c75eae483a5a
NghiNV <ngonghihp@gmail.com>
2016-09-17 21:06:18 +0900

    dev db config

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -6,5 +6,11 @@
  */
 
 return array(
-
+    'default' => array(
+        'connection'  => array(
+            'dsn'        => 'mysql:host=localhost;dbname=vivino',
+            'username'   => 'homestead',
+            'password'   => 'secret',
+        ),
+    ),
 );
